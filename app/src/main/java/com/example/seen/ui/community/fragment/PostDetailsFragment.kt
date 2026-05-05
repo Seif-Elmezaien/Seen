2be4295a58e7205a93a6c3ecd573ad32007e6e99
Sarah Elzeiny <zeinysarah@gmail.com>
2026-05-06 02:11:27 +0300

    details post fragment item comment

---
+++ app/src/main/java/com/example/seen/ui/community/fragment/PostDetailsFragment.kt
@@ -0,0 +1,35 @@
+package com.example.seen.ui.community.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.seen.R
+import com.example.seen.databinding.FragmentCommunityBinding
+import com.example.seen.databinding.FragmentPostDetailsBinding
+
+
+class PostDetailsFragment : Fragment() {
+
+    var _binding: FragmentCommunityBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCommunityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
