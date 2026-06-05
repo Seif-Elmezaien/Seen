35625b468d64fd52ec2d0e0e07818a26654b5d6b
Seif eldin Nabil <153958903+Seif-Elmezaien@users.noreply.github.com>
2026-06-05 11:47:32 +0300

    Chatbot Design

---
+++ app/src/main/java/com/example/seen/ui/home/fragment/ChatbotFragment.kt
@@ -0,0 +1,33 @@
+package com.example.seen.ui.home.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.seen.databinding.FragmentChatbotBinding
+
+
+class ChatbotFragment : Fragment() {
+    var _binding: FragmentChatbotBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatbotBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
